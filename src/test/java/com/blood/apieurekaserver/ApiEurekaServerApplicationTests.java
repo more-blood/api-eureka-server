9696ce25a282d429b1346259ad79603f23f78b6a
Raphael Freitas <dev.rapha@gmail.com>
2018-06-19 17:05:51 -0300

    initialize eureka server

---
+++ src/test/java/com/blood/apieurekaserver/ApiEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blood.apieurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
